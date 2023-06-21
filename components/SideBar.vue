119b1b35f51ef06e936de3f38ed480522429e981
Carlos Muñoz <carmuny54@gmail.com>
2023-06-21 08:58:42 +0200

    Kick Clone

---
+++ components/SideBar.vue
@@ -0,0 +1,76 @@
+<template>
+    <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-[#191B1F] sm:translate-x-0" aria-label="Sidebar">
+      <div class="h-full px-3 pb-4 overflow-y-auto bg-[#191B1F]">
+          <ul class="space-y-2 font-medium">
+            <li class="text-white">
+                Following
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img class="w-8 h-8 w-[15%] text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white rounded-full" src="https://files.kick.com/images/user/676/profile_image/conversion/931b4e8f-5445-427c-bd82-b473530390cc-thumb.webp" alt="" srcset="">
+                    <div class="flex flex-col overflow-hidden w-[55%]">
+                        <span class="ml-3">xQc</span>
+                        <span class="ml-3 text-[#7BADC3] whitespace-nowrap text-ellipsis overflow-hidden">Other, Whatch Party</span>
+                    </div>
+                    <span class="text-[14px] text-right w-[30%]">
+                        56,232
+                    </span>
+                </a>
+            </li>
+
+            <li class="text-white">
+                Recommended
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img class="w-8 h-8 w-[15%] text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white rounded-full" src="https://files.kick.com/images/user/4647/profile_image/conversion/7e18ea54-0842-48c8-8c6b-1372dfd990f6-thumb.webp" alt="" srcset="">
+                    <div class="flex flex-col overflow-hidden w-[55%]">
+                        <span class="ml-3">ROSHTEIN</span>
+                        <span class="ml-3 text-[#7BADC3] whitespace-nowrap text-ellipsis overflow-hidden">Slots & Casino</span>
+                    </div>
+                    <span class="text-[14px] text-right w-[30%]">
+                    56,232
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img class="w-8 h-8 w-[15%] text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white rounded-full" src="https://files.kick.com/images/user/336946/profile_image/conversion/95f165f9-dfe4-4ea2-a3a1-0e91d2f2263c-thumb.webp" alt="" srcset="">
+                    <div class="flex flex-col overflow-hidden w-[55%]">
+                        <span class="ml-3">Sam</span>
+                        <span class="ml-3 text-[#7BADC3] whitespace-nowrap text-ellipsis overflow-hidden">Just Chatting</span>
+                    </div>
+                    <span class="text-[14px] text-right w-[30%]">
+                    56,232
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img class="w-8 h-8 w-[15%] text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white rounded-full" src="https://files.kick.com/images/user/82334/profile_image/conversion/678d32ab-1d3f-4930-8a94-de115cccf9ec-thumb.webp" alt="" srcset="">
+                    <div class="flex flex-col overflow-hidden w-[55%]">
+                        <span class="ml-3">Ryda</span>
+                        <span class="ml-3 text-[#7BADC3] whitespace-nowrap text-ellipsis overflow-hidden">Call of Duty: Warzone 2.0</span>
+                    </div>
+                    <span class="text-[14px] text-right w-[30%]">
+                    56,232
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <img class="w-8 h-8 w-[15%] text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white rounded-full" src="https://files.kick.com/images/user/442754/profile_image/conversion/801dbde9-cea8-41cf-a0db-c3ee75b48dd2-thumb.webp" alt="" srcset="">
+                    <div class="flex flex-col overflow-hidden w-[55%]">
+                        <span class="ml-3">Kate</span>
+                        <span class="ml-3 text-[#7BADC3] whitespace-nowrap text-ellipsis overflow-hidden">House Flipper</span>
+                    </div>
+                    <span class="text-[14px] text-right w-[30%]">
+                    56,232
+                    </span>
+                </a>
+            </li>
+
+          </ul>
+      </div>
+    </aside>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  
+  <div>
+    <Header></Header>
+    <SideBar></SideBar>
+    <div class="sm:ml-64">
+      <div class=" mt-14">
+        test
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+*{
+  font-family: 'Inter', sans-serif;
+}
+
+body{
+  background-color: #0B0E0F;
+}
+
+</style>
